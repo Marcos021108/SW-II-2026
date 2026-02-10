a701c8ac9c7a89d9cddae421ae333be0b49c4742
Marcos <161627757+Marcos021108@users.noreply.github.com>
2026-02-10 17:30:42 -0300

    Add files via upload

---
+++ Aula01/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = "Marcos";
+    $idade = 17;
+    $preco = 79.50;
+    $ativo = true;
+    echo"Nome: ".$nome." <br>";
+    echo"Idade: ". $idade . "<br>";
+    echo"Preço: ". $preco ."<br>";
+    echo"Status: ". ($ativo ? "Ativo" : "Inativo");
+
+?>
